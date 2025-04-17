cf7c1e14dd5bcbe6e6e87dbaee47bb3f86f64d1a
mugeon <pos04167@kakao.com>
2025-04-18 00:19:53 +0900

    [refactor] 결제 코드 정리
    # Conflicts:
    #	src/main/java/kr/hhplus/be/server/domain/payment/PaymentEntity.java
    #	src/main/java/kr/hhplus/be/server/domain/payment/PaymentRepository.java
    #	src/main/java/kr/hhplus/be/server/domain/payment/PaymentService.java

---
+++ src/main/java/kr/hhplus/be/server/domain/payment/PaymentService.java
@@ -0,0 +1,46 @@
+package kr.hhplus.be.server.domain.payment;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+
+    @Transactional
+    public void processPayment(Long orderId, Long userId, BigDecimal amount) {
+        try {
+            PaymentEntity payment = createPayment(orderId, userId, amount);
+            payment.complete();
+            paymentRepository.save(payment);
+        } catch (Exception e) {
+            failPayment(orderId, userId, amount, e.getMessage());
+            throw new RuntimeException("결제 처리 중 오류가 발생했습니다: " + e.getMessage(), e);
+        }
+    }
+
+    public PaymentEntity createPayment(Long orderId, Long userId, BigDecimal amount) {
+        return PaymentEntity.create(
+                orderId,
+                userId,
+                amount
+        );
+    }
+
+    public PaymentEntity failPayment(Long orderId, Long userId, BigDecimal amount, String errorMessage) {
+        PaymentEntity payment = PaymentEntity.create(
+                orderId,
+                userId,
+                amount
+        );
+        payment.fail();
+        return paymentRepository.save(payment);
+    }
+
+}
